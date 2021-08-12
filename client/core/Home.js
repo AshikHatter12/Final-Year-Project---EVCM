59da73b32555f4b3b5b5cc0478afd996157d4485
unknown <smrusmruthi@gmail.com>
2021-08-12 10:54:23 +0530

    diploy2j

--- client/core/Home.js
+++ client/core/Home.js
@@ -14,7 +14,7 @@ function Copyright() {
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
       <Link color="inherit" href="https://www.bietdvg.edu/">
-        BIET DVG Enterprise Video Content Management
+        BIET DVG Enterprise Video Content Management 2
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
